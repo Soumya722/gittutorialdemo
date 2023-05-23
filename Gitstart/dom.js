3f440caf4a2375a20fc9ac64b026652599997576
Soumya722 <soumyaranjanbehera722@gmail.com>
2023-05-24 00:22:25 +0530

    TagName

--- Gitstart/dom.js
+++ Gitstart/dom.js
@@ -23,16 +23,30 @@
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML ='<h3>Hello</h3>';
 //headerTitle.style.borderBottom = 'solid 3px #000';
- 
-//GETELEMENTSBYCLASSNAME
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'hello 2';
-//items[1].style.fontweight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items.style.fontweight = 'bold';
-for(var i=0;i<items.length;i++){
-    items[i].style.backgroundColor = ' #f4f4f4 ';
-}
-items[2].style.backgroundColor = ' green '
+//GETELEMENTSBYCLASSNAME//
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'hello 2';
+// items[1].style.backgroundColor = 'yellow';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor = ' #f4f4f4 ';
+// }
+// items[2].style.backgroundColor = ' green '
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'hello 2';
+// li[1].style.fontweight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// for (var i = 0; i < li.length; i++) {
+//     li[i].style.backgroundColor = ' #f4f4f4 ';
+// }
+
+
+//QureySelector
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc'
+
+var input = document.querySelector('input');
+input.value = 'Hello World' 
