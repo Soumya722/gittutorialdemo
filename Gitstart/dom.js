227b84ccccc78f0a4b79e82bdb1b9d21430e427a
Soumya722 <soumyaranjanbehera722@gmail.com>
2023-05-23 19:43:27 +0530

    index

--- Gitstart/dom.js
+++ Gitstart/dom.js
@@ -15,11 +15,24 @@
 
 //getElementById
 //console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var headerTitle = document.getElementById('main-header');
+//var headerTitle = document.getElementById('header-title');
+//var headerTitle = document.getElementById('main-header');
 //console.log(headerTitle);
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML ='<h3>Hello</h3>';
-headerTitle.style.borderBottom = 'solid 3px #000';
+//headerTitle.style.borderBottom = 'solid 3px #000';
+ 
+//GETELEMENTSBYCLASSNAME
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hello 2';
+//items[1].style.fontweight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+items.style.fontweight = 'bold';
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor = ' #f4f4f4 ';
+}
+items[2].style.backgroundColor = ' green '
