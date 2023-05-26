2cf2bc8754588d6226cf7caf7bad67dd526d9398
Soumya722 <soumyaranjanbehera722@gmail.com>
2023-05-26 13:18:57 +0530

    more user

--- Gitstart/js/main.js
+++ Gitstart/js/main.js
@@ -1,13 +1,15 @@
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const phoneNoInput = document.querySelector('#phoneNo');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#user');
+const list =document.getElementById('list');
 
 myForm.addEventListener('submit',onSubmit);
 function onSubmit(e){
     e.preventDefault();
-    if(nameInput.value === '' || emailInput.value ===''){
+    if(nameInput.value === '' || emailInput.value ==='' ||phoneNoInput.value ===''){
         msg.classList.add('error');
         msg.innerHTML ='please enter all fields';
         setTimeout(() => msg.remove(), 3000);
@@ -15,10 +17,14 @@ function onSubmit(e){
         const obj={
             name:nameInput.value,
             email:emailInput.value,
+            PhoneNo:phoneNoInput.value,
         };
         var objString = JSON.stringify(obj);
         localStorage.setItem(email.value , objString);
         console.log(nameInput.value , emailInput.value);
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value} ${phoneNoInput.value}`));
+        list.appendChild(li);
     }
     
 }
